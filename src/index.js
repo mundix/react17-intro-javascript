ff569a9175e8cc7849381112ebc200c3642983f0
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-09-23 05:34:40 -0400

    Trabajando con async y await

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 // Async - Await
 import './css/index.css';
 
+// const getImagenPromesa = () => new Promise( resolve => resolve('http://aslkdalsdkjaldj.com'));
+// getImagenPromesa().then( console.log);
+
+// Ahora utilizando async
+const getImage = async() => {
+
+
+    try {
+        
+        const apiKey = `63J5cK4xMY25TfnCcrJzJJn6W06Ed4Xc`;
+        const respuesta = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+        const { data } = await respuesta.json();
+        const { url } =  data.images.original;
+    
+        const img = document.createElement('img');
+        img.src = url;
+        document.body.append(img);
+
+    } catch (error) {
+        // Manejo de error
+        console.log(error);
+    }
+
+   
+
+}
+
+// console.log(getImage());
+getImage();
+
+
+// 
+// 
+
+// peticiones.then( resp => resp.json())
+// .then( ({ data }) => { 
+//     const { url } = data.images.original;
+//     const img = document.createElement('img');
+//     img.src = url;
+//     document.body.append(img);
+//  })
+// .catch( console.log );
